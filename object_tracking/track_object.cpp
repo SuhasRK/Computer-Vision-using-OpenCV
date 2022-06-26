9e3e5192f2987282cbdadea8cd46cf739e7db20d
SuhasRK <suhasrk233@gmail.com>
2022-06-26 23:18:27 +0530

    object_tracking cotinued

--- object_tracking/track_object.cpp
+++ object_tracking/track_object.cpp
@@ -1,1 +1,29 @@
-// to be continue;
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
+#include <iostream>
+
+#include<opencv2/tracking.hpp>
+#include<opencv2/core/ocl.hpp>
+
+using namespace cv;
+using namespace std;
+
+// Convert to string
+#define SSTR( x ) static_cast< std::ostringstream & >( \
+( std::ostringstream() << std::dec << x ) ).str()
+
+
+
+int main(int argc,char **argv){
+    string trackTypes[8] = {"BOOSTING","MIL","KCF","TLD","MEDIANFLOW", "GOTURN", "MOSSE", "CSRT"};
+
+    string trackerType=trackTypes[2];
+
+    Ptr<Tracker> tracker;
+
+    if (CV_MINOR_VERSION<3){
+        tracker = Tracker::create(trackerType);
+    }
+
+
+}
